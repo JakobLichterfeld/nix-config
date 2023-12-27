da678a6fb91f8d071ddc7bdf1acc7a9f59ca40f4
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2023-12-27 11:03:36 +0100

    fix: Update home-manager version and import syntax

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -1,7 +1,7 @@
 { inputs, pkgs, lib, ... }:
 {
   imports = [
-    home-manager/nix-darwin
+    <home-manager/nix-darwin>
     ../../modules/cachix
     ./dock
     ./system.nix
