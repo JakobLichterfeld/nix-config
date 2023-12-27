f5879b615ef74d4dc07ca2fbea4ebccf3c16729f
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2023-12-27 11:34:33 +0100

    feat: Update flake.nix with new repository URLs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Configuration for MacOS and NixOS";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/master";
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
