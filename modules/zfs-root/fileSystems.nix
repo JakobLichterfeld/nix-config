28e75ff8e95a3eb3d6e8e4a1af6ce4d0d4d59d07
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-01 18:01:07 +0100

    fix: device path in fileSystems.nix

--- modules/zfs-root/fileSystems.nix
+++ modules/zfs-root/fileSystems.nix
@@ -41,7 +41,7 @@ in {
     };
   }) cfg.bindmounts ++ map (esp: {
     "/boot/efis/${esp}" = {
-      device = "${config.zfs-root.boot.devNodes}/${esp}";
+      device = "${config.zfs-root.boot.devNodes}${esp}";
       fsType = "vfat";
       options = [
         "x-systemd.idle-timeout=1min"
@@ -54,7 +54,7 @@ in {
     };
   }) cfg.efiSystemPartitions);
   config.swapDevices = mkDefault (map (swap: {
-    device = "${config.zfs-root.boot.devNodes}/${swap}";
+    device = "${config.zfs-root.boot.devNodes}${swap}";
     discardPolicy = mkDefault "both";
     randomEncryption = {
       enable = true;
