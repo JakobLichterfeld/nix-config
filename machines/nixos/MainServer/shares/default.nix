878df3af9eec807909f6c4caa3956eed35964e3d
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-03 14:19:35 +0100

    style: increas readability by correct tabbing

--- machines/nixos/MainServer/shares/default.nix
+++ machines/nixos/MainServer/shares/default.nix
@@ -23,77 +23,77 @@ let
   smb_shares = builtins.mapAttrs (name: value: value // smb.share_params) smb.share_list;
 in
 {
-services.samba-wsdd.enable = true; # enable wsdd for discovery
+  services.samba-wsdd.enable = true; # enable wsdd for discovery
 
-users = {
-groups.share = {
-  gid = 993;
-};
-users.share = {
-  uid = 994;
-  isSystemUser = true;
-  group = "share";
-};
-};
-
-environment.systemPackages = [ config.services.samba.package ];
-
-users.users.jakob.extraGroups = ["share"];
-
-systemd.tmpfiles.rules = map (x: "d ${x.path} 0775 share share - -") (lib.attrValues smb.share_list) ++ ["d /mnt 0775 share share - -"];
-
-system.activationScripts.samba_user_create = ''
-    smb_password=$(cat "${config.age.secrets.sambaPassword.path}")
-    echo -e "$smb_password\n$smb_password\n" | /run/current-system/sw/bin/smbpasswd -a -s share
-    '';
-
-networking.firewall = {
-  allowedTCPPorts = [ 5357 ]; # Microsoft Network Discovery,
-  allowedUDPPorts = [ 3702 ]; # Web Services Discovery (WSD)
-};
-
-services.samba = {
-  enable = true;
-  openFirewall = true;
-  invalidUsers = [
-    "root"
-  ];
-  securityType = "user";
-  extraConfig = ''
-    workgroup = WORKGROUP
-    server string = ${config.age.secrets.MainServer_hostName.path}
-    netbios name = ${config.age.secrets.MainServer_hostName.path}
-    security = user
-    hosts allow = ${config.age.secrets.MainServer_ipNetwork.path}
-    guest account = nobody
-    map to guest = bad user
-    passdb backend = tdbsam
-    '';
-  shares = smb_shares;
-};
-services.avahi = {
-  enable = true;
-  nssmdns = true;
-  publish = {
-    enable = true;
-    addresses = true;
-    domain = true;
-    hinfo = true;
-    userServices = true;
-    workstation = true;
+  users = {
+    groups.share = {
+      gid = 993;
+    };
+    users.share = {
+      uid = 994;
+      isSystemUser = true;
+      group = "share";
+    };
   };
-  extraServiceFiles = {
-    smb = ''
-      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-      <service-group>
-      <name replace-wildcards="yes">%h</name>
-      <service>
-      <type>_smb._tcp</type>
-      <port>445</port>
-      </service>
-      </service-group>
+
+  environment.systemPackages = [ config.services.samba.package ];
+
+  users.users.jakob.extraGroups = ["share"];
+
+  systemd.tmpfiles.rules = map (x: "d ${x.path} 0775 share share - -") (lib.attrValues smb.share_list) ++ ["d /mnt 0775 share share - -"];
+
+  system.activationScripts.samba_user_create = ''
+      smb_password=$(cat "${config.age.secrets.sambaPassword.path}")
+      echo -e "$smb_password\n$smb_password\n" | /run/current-system/sw/bin/smbpasswd -a -s share
       '';
+
+  networking.firewall = {
+    allowedTCPPorts = [ 5357 ]; # Microsoft Network Discovery,
+    allowedUDPPorts = [ 3702 ]; # Web Services Discovery (WSD)
   };
-};
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    invalidUsers = [
+      "root"
+    ];
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = ${config.age.secrets.MainServer_hostName.path}
+      netbios name = ${config.age.secrets.MainServer_hostName.path}
+      security = user
+      hosts allow = ${config.age.secrets.MainServer_ipNetwork.path}
+      guest account = nobody
+      map to guest = bad user
+      passdb backend = tdbsam
+      '';
+    shares = smb_shares;
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+        <name replace-wildcards="yes">%h</name>
+        <service>
+        <type>_smb._tcp</type>
+        <port>445</port>
+        </service>
+        </service-group>
+        '';
+    };
+  };
 }
