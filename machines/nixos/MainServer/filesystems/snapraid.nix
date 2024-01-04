4b9373cc3c8d6b8daf292c3609421d6ebe755a08
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-04 17:06:13 +0100

    fix: convert snapraid systemd units to boot.initrd.systemd as they support postDeviceCommands

--- machines/nixos/MainServer/filesystems/snapraid.nix
+++ machines/nixos/MainServer/filesystems/snapraid.nix
@@ -1,49 +1,51 @@
 { inputs, config, pkgs, lib, ... }:
 {
-snapraid = {
-  enable = true;
-  parityFiles = [
-    "/mnt/parity1/snapraid.parity"
-  ];
-  contentFiles = [
-    "/mnt/data1/snapraid.content"
-  ];
-  dataDisks = {
-    d1 = "/mnt/data1";
+  snapraid = {
+    enable = true;
+    parityFiles = [
+      "/mnt/parity1/snapraid.parity"
+    ];
+    contentFiles = [
+      "/mnt/data1/snapraid.content"
+    ];
+    dataDisks = {
+      d1 = "/mnt/data1";
+    };
+    exclude = [
+      "*.unrecoverable"
+        "/tmp/"
+        "/lost+found/"
+    ];
   };
-  exclude = [
-    "*.unrecoverable"
-      "/tmp/"
-      "/lost+found/"
-  ];
-};
 
-systemd.services.snapraid-sync = {
-  serviceConfig = {
-    RestrictNamespaces = lib.mkForce false;
-    RestrictAddressFamilies = lib.mkForce "";
-  };
-  postStop = ''
-  if [[ $SERVICE_RESULT =~ "success" ]]; then
-    message=""
-  else
-    message=$(journalctl --unit=snapraid-sync.service -n 20 --no-pager)
-  fi
-  /run/current-system/sw/bin/notify -s "$SERVICE_RESULT" -t "Snapraid Sync" -m "$message"
-  '';
-};
+  boot.initrd.systemd = {
+    services.snapraid-sync = {
+      serviceConfig = {
+        RestrictNamespaces = lib.mkForce false;
+        RestrictAddressFamilies = lib.mkForce "";
+      };
+      postStop = ''
+      if [[ $SERVICE_RESULT =~ "success" ]]; then
+        message=""
+      else
+        message=$(journalctl --unit=snapraid-sync.service -n 20 --no-pager)
+      fi
+      /run/current-system/sw/bin/notify -s "$SERVICE_RESULT" -t "Snapraid Sync" -m "$message"
+      '';
+    };
 
-systemd.services.snapraid-scrub = {
-  serviceConfig = {
-    RestrictAddressFamilies = lib.mkForce "";
+    services.snapraid-scrub = {
+      serviceConfig = {
+        RestrictAddressFamilies = lib.mkForce "";
+      };
+      postStop = ''
+      if [[ $SERVICE_RESULT =~ "success" ]]; then
+        message=""
+      else
+        message=$(journalctl --unit=snapraid-scrub.service -n 20 --no-pager)
+      fi
+      /run/current-system/sw/bin/notify -s "$SERVICE_RESULT" -t "Snapraid Scrub" -m "$message"
+      '';
+    };
   };
-  postStop = ''
-  if [[ $SERVICE_RESULT =~ "success" ]]; then
-    message=""
-  else
-    message=$(journalctl --unit=snapraid-scrub.service -n 20 --no-pager)
-  fi
-  /run/current-system/sw/bin/notify -s "$SERVICE_RESULT" -t "Snapraid Scrub" -m "$message"
-  '';
-};
 }
