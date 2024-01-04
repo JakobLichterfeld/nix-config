4d87b68153475d1ed5283680ecb43b8baf65d421
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-04 14:06:37 +0100

    feat: Update home-manager and agenix dependencies

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
