7c79bb56df89f8baa2f7aab91ef4893d76b1eec2
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-05 16:30:26 +0100

    chore: Update passwordFile variable name to hashedPasswordFile

--- users/jakob/default.nix
+++ users/jakob/default.nix
@@ -13,7 +13,7 @@
         home = "/Users/jakob";
         uid = 1000;
         isNormalUser = true;
-        passwordFile = config.age.secrets.hashedUserPassword.path;
+        hashedPasswordFile = config.age.secrets.hashedUserPassword.path;
         extraGroups = [ "wheel" "users" "video" ];
         group = "jakob";
         openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOquQ/e3s3yYUYjwk2vth18wWGTNlOmNUzjPXUzKeXZI 20231225_jakob_lichterfeld" ];
