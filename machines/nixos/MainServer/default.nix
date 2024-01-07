5fbf744eb32394a5e1298ecb4d7f05c8c69b0f05
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-07 13:31:00 +0100

    fix: correct use of default values for hostName, hostId and timeZone

--- machines/nixos/MainServer/default.nix
+++ machines/nixos/MainServer/default.nix
@@ -23,10 +23,10 @@
   boot.initrd.availableKernelModules = [  "uhci_hcd" "ehci_pci" "ahci" "sd_mod" "nvme" "uas" "nvme" "ahci" "i915" ];
   boot.kernelParams = [ "consoleblank=60" ];
   networking = {
-    hostName = machinesSensitiveVars.MainServer.hostName;
-    hostId = machinesSensitiveVars.MainServer.hostId;
+    hostName = lib.mkForce machinesSensitiveVars.MainServer.hostName;
+    hostId = lib.mkForce machinesSensitiveVars.MainServer.hostId;
   };
-  time.timeZone = "Europe/Berlin";
+  time.timeZone = lib.mkForce "Europe/Berlin";
 
 
   imports = [
