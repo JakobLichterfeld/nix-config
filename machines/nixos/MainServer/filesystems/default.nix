e584dbbc50b80d36b3a3d971c8d42c81c40a93cf
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-07 13:25:07 +0100

    fix: force zfs filesystem for /etc/nixos

--- machines/nixos/MainServer/filesystems/default.nix
+++ machines/nixos/MainServer/filesystems/default.nix
@@ -58,7 +58,7 @@
     neededForBoot = true;
   };
 
-  fileSystems."/etc/nixos" =
+  fileSystems."/etc/nixos" = lib.mkForce
   { device = "rpool/nixos/config";
     fsType = "zfs";
     neededForBoot = true;
