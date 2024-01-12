01df91651f95189eabcd08844085855ecbf04cfb
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-12 14:13:55 +0100

    fix: Update SSH host keys generation in NixOS configuration

--- machines/nixos/default.nix
+++ machines/nixos/default.nix
@@ -32,8 +32,13 @@
     ports = [ machinesSensitiveVars.MainServer.sshPort ];
     hostKeys = [
       {
-        path = "/persist/ssh/id_ed25519_main_server";
+        path = "/persist/ssh/ssh_host_ed25519_key";
         type = "ed25519";
+      }
+      {
+        path = "/persist/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
       }
     ];
   };
