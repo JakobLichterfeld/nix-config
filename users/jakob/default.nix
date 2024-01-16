c441b4f2e3c0b8500fde98b01f9905ef76b49abd
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-01-16 18:06:58 +0100

    refactor: remove unused age.identityPaths in user config

--- users/jakob/default.nix
+++ users/jakob/default.nix
@@ -4,7 +4,7 @@
 
   nix.settings.trusted-users = [ "jakob" ];
 
-  age.identityPaths = ["/Users/jakob/.ssh/id_ed25519"];
+  # age.identityPaths = ["/Users/jakob/.ssh/id_ed25519"];
 
   users = {
     users = {
