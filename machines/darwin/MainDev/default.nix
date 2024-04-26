c1a1501f028ceb8faea5530100b25b8d0a62e15f
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-04-26 09:40:02 +0200

    refactor: remove unused ansible python config

--- machines/darwin/MainDev/default.nix
+++ machines/darwin/MainDev/default.nix
@@ -1,26 +1,10 @@
 { inputs, pkgs, lib, ... }:
 let
-  ourPythonPackagesForAnsible = pkgs.python311Packages.override
-    (oldAttrs: {
-      overrides = pkgs.lib.composeManyExtensions [
-        (oldAttrs.overrides or (_: _: { }))
-        (pfinal: pprev: {
-          ansible = pprev.ansible.overridePythonAttrs (old: {
-            propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ pfinal.boto pfinal.boto3 pfinal.pyyaml ];
-            makeWrapperArgs = (old.makeWrapperArgs or []) ++ [ "--prefix PYTHONPATH : $PYTHONPATH" ];
-          });
-        })
-      ];
-    });
-  ourAnsible =
-    (ourPythonPackagesForAnsible.toPythonApplication ourPythonPackagesForAnsible.ansible);
-
   masApps = import ./masApps.nix;
   brews = import ./brews.nix;
   casks = import ./casks.nix;
 in
 {
-
   homebrew = {
     masApps = masApps;
     brews = brews;
@@ -33,5 +17,4 @@ in
 
 
   environment.systemPackages = pkgs.callPackage ./packages.nix {};
-
-  }
+}
