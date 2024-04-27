bccdceb29a47d0348b0396cfb0383e5d80429eda
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-04-27 09:54:11 +0200

    feat: rollback zfs rpool datasets

--- machines/nixos/MainServer/filesystems/default.nix
+++ machines/nixos/MainServer/filesystems/default.nix
@@ -41,6 +41,28 @@
       fsType = "zfs";
     };
 
+  boot.initrd.systemd.services.rollback = {
+    description = "Rollback ZFS datasets to a pristine state";
+    wantedBy = [
+      "initrd.target"
+    ];
+    after = [
+      "zfs-import-zroot.service"
+    ];
+    before = [
+      "sysroot.mount"
+    ];
+    path = with pkgs; [
+      zfs
+    ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      zfs rollback -r rpool/nixos/empty@start
+    '';
+  };
+
+
   fileSystems."/nix" = lib.mkForce
     {
       device = "rpool/nixos/nix";
