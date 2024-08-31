d8ac1e12c16e9fee63b1458047d15520e3b696bd
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-08-31 11:48:25 +0200

    feat: Add orbstack for running containers

--- machines/darwin/MainDev/brews.nix
+++ machines/darwin/MainDev/brews.nix
@@ -2,6 +2,7 @@
   # Development
   { name = "rustup"; } # Rust toolchain installer
   { name = "lcov"; } # Code coverage
+  { name = "orbstack"; } # Fast, light, powerful way to run containers
 
   # Encryption and security tools
   { name = "ophcrack"; }
