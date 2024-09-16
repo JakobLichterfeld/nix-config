c1c507f89f6399ae7489b68aae5430a122686472
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-09-16 14:48:44 +0200

    feat: add clang-format for code formatting

--- machines/darwin/MainDev/brews.nix
+++ machines/darwin/MainDev/brews.nix
@@ -6,6 +6,7 @@
   { name = "treefmt"; } # One CLI to format the code tree
   { name = "yamlfmt"; } # Extensible command-line tool to format YAML files
   { name = "prettier"; } # Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML
+  { name = "clang-format"; } # Formatting tools for C, C++, Obj-C, Java, JavaScript, TypeScript
 
   # Encryption and security tools
   { name = "ophcrack"; }
