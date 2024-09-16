b1231e0ac90dfb94ac7733377d4a0f1e844ce98b
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2024-09-16 11:53:59 +0200

    feat: add treefmt and yamlfmt

--- machines/darwin/MainDev/brews.nix
+++ machines/darwin/MainDev/brews.nix
@@ -3,6 +3,8 @@
   { name = "rustup"; } # Rust toolchain installer
   { name = "lcov"; } # Code coverage
   { name = "orbstack"; } # Fast, light, powerful way to run containers
+  { name = "treefmt"; } # One CLI to format the code tree
+  { name = "yamlfmt"; } # Extensible command-line tool to format YAML files
 
   # Encryption and security tools
   { name = "ophcrack"; }
