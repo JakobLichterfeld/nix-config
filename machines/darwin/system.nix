7e94a77ae640ba5af87c554e6b4ac112d31ec763
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-03-19 14:12:26 +0100

    feat(darwin): Link Nix-installed applications to /Applications for spotlight search integration

--- machines/darwin/system.nix
+++ machines/darwin/system.nix
@@ -27,6 +27,30 @@
   };
   system = {
     stateVersion = 4;
+
+    # link nix installed applications to /Applications
+    # This results in the apps installed by nix to be available in spotlight search as well
+    activationScripts.applications.text =
+      let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+      pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
+
     defaults = {
       trackpad = {
         Clicking = true;
