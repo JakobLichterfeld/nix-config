10e2114189140da0ec89ec5ed8fd9ebf76e2c2f7
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-03-21 11:43:33 +0100

    feat(darwin): use homebrew for rectange to get latest version

--- machines/darwin/packages.nix
+++ machines/darwin/packages.nix
@@ -73,7 +73,6 @@ with pkgs;
 
   # System tools
   alt-tab-macos # macOS alt-tab replacement
-  rectangle # Move and resize windows in macOS using keyboard shortcuts or snap areas
   android-file-transfer
   karabiner-elements # Keyboard customizer for macOS
   mkalias # Create and manage aliases for files and directories
