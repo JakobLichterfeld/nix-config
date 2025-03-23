8bdd595d6fc9e07559023f2a44fbd770f7e68f9a
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-03-23 13:22:46 +0100

    feat: add trusted substituters and public keys to configuration

--- flake.nix
+++ flake.nix
@@ -1,5 +1,23 @@
 {
   description = "Configuration for MacOS and NixOS";
+
+    nixConfig = {
+      # only use trusted substituters
+    trusted-substituters = [
+      "https://cachix.cachix.org"
+      "https://nixpkgs.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    # only use trusted public keys
+    # this is a list of public keys that are used to sign the substituters
+    trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
