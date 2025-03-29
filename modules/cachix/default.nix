a795e2d090384224b6b36f5755c5f8f1d04d6f98
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-03-29 12:01:36 +0100

    refactor(cachix): move trusted substituters and public keys to module

--- modules/cachix/default.nix
+++ modules/cachix/default.nix
@@ -2,10 +2,16 @@
 {
   nix.settings = {
     substituters = [
+      "https://cachix.cachix.org"
+      "https://nixpkgs.cachix.org"
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
     ];
+    # only use trusted public keys
+    # this is a list of public keys that are used to sign the substituters
     trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
