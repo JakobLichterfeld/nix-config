092fcb2702a985f28e8df174cc2f6f726bc22e29
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-04-29 17:12:28 +0200

    refactor(tailscale): use new authKeyFile setting for autostart

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -15,47 +15,6 @@
 
   services.tailscale = {
     enable = true;
-  };
-
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    after = [
-      "network-pre.target"
-      "tailscale.service"
-    ];
-    wants = [
-      "network-pre.target"
-      "tailscale.service"
-    ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      LoadCredential = [
-        "TAILSCALE_AUTH_KEY_FILE:${config.age.secrets.tailscaleAuthKey.path}"
-      ];
-    };
-
-    script = with pkgs; ''
-
-      # wait for tailscaled to settle
-      echo "Waiting for tailscale.service start completion ..."
-      sleep 5
-      # (as of tailscale 1.4 this should no longer be necessary, but I find it still is)
-
-      # check if already authenticated
-      echo "Checking if already authenticated to Tailscale ..."
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then  # do nothing
-      	echo "Already authenticated to Tailscale, exiting."
-        exit 0
-      fi
-
-      echo "Authenticating with Tailscale ..."
-      # --advertise-exit-node
-      export TAILSCALE_AUTH_KEY=$(${pkgs.systemd}/bin/systemd-creds cat TAILSCALE_AUTH_KEY_FILE)
-      ${tailscale}/bin/tailscale up --auth-key "$TAILSCALE_AUTH_KEY"
-    '';
+    authKeyFile = config.age.secrets.tailscaleAuthKey.path;
   };
 }
