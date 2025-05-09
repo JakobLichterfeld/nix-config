d515742a635f9b44723eb6722588cf23ee6b8fa3
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-05-09 18:39:51 +0200

    feat(MainServer): add homelab configuration with Samba and services setup

---
+++ machines/nixos/MainServer/homelab/default.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  lib,
+  vars,
+  machinesSensitiveVars,
+  ...
+}:
+let
+  hl = config.homelab;
+in
+{
+  homelab = {
+    enable = true;
+    baseDomain = machinesSensitiveVars.MainServer.baseDomain;
+    baseDomainFallback = machinesSensitiveVars.MainServer.baseDomainFallback;
+
+    timeZone = "Europe/Berlin";
+    mounts = {
+      config = vars.serviceConfigRoot;
+      slow = vars.slowerArray;
+      fast = vars.cacheArray;
+      merged = vars.mainArray;
+    };
+    samba = {
+      enable = true;
+      passwordFile = config.age.secrets.sambaPassword.path;
+      shares = {
+        Backups = {
+          path = "${hl.mounts.merged}/Backups";
+        };
+        Documents = {
+          path = "${hl.mounts.fast}/Documents";
+        };
+        Media = {
+          path = "${hl.mounts.merged}/Media";
+        };
+        Music = {
+          path = "${hl.mounts.fast}/Media/Music";
+        };
+        Misc = {
+          path = "${hl.mounts.merged}/Misc";
+        };
+        TimeMachine = {
+          path = "${hl.mounts.fast}/TimeMachine";
+          "fruit:time machine" = "yes";
+        };
+      };
+    };
+    services = {
+      enable = true;
+      homepage = {
+        enable = true;
+        misc = [
+          {
+            FritzBox = {
+              href = "http://${machinesSensitiveVars.MainServer.defaultGateway}";
+              siteMonitor = "http://${machinesSensitiveVars.MainServer.defaultGateway}";
+              description = "DSL/Cable Modem WebUI";
+              icon = "avm-fritzbox.png";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
