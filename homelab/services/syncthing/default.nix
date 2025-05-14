8a341812f9b352f1babb37f4eb25e68897cda7f3
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-05-14 13:51:33 +0200

    feat(syncthing): add homepage config

--- homelab/services/syncthing/default.nix
+++ homelab/services/syncthing/default.nix
@@ -9,21 +9,45 @@ let
   service = "syncthing";
   cfg = config.homelab.services.syncthing;
   homelab = config.homelab;
-  configDir = "${vars.serviceConfigRoot}/syncthing";
-  directories = [
-  configDir
-];
 in
 {
-  options.homelab.services.syncthing = {
+  options.homelab.services.${service} = {
     enable = lib.mkEnableOption {
       description = "Enable ${service}";
+    };
+    configDir = lib.mkOption {
+      type = lib.types.str;
+      default = "${vars.serviceConfigRoot}/${service}";
+    };
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "${vars.mainArray}/sync";
+    };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "${service}.${homelab.baseDomain}";
+    };
+    homepage.name = lib.mkOption {
+      type = lib.types.str;
+      default = "${service}";
+    };
+    homepage.description = lib.mkOption {
+      type = lib.types.str;
+      default = "Continuous File Synchronization";
+    };
+    homepage.icon = lib.mkOption {
+      type = lib.types.str;
+      default = "syncthing.png";
+    };
+    homepage.category = lib.mkOption {
+      type = lib.types.str;
+      default = "Services";
     };
   };
   config = lib.mkIf cfg.enable {
 
     # Create directories for Syncthing with the correct permissions and ownership.
-    systemd.tmpfiles.rules = map (x: "d ${x} 0775 ${homelab.user} ${homelab.group} - -") directories;
+    systemd.tmpfiles.rules = [ "d ${cfg.configDir} 0775 ${homelab.user} ${homelab.group} - -" ];
 
     # Syncthing ports: 8384 for remote access to GUI
     # 22000 TCP and/or UDP for sync traffic
@@ -42,14 +66,14 @@ in
 
       group = homelab.group; # Group to run Syncthing as
       user = homelab.user; # User to run Syncthing as
-      dataDir = "${vars.mainArray}/sync"; # Default folder for new synced folders
-      configDir = configDir; # Folder for Syncthing's settings and keys
+      dataDir = cfg.dataDir; # Default folder for new synced folders
+      configDir = cfg.configDir; # Folder for Syncthing's settings and keys
       guiAddress = "0.0.0.0:8384"; # Listen on all interfaces
       overrideFolders = false;
       overrideDevices = false;
     };
 
-    services.caddy.virtualHosts."${homelab.baseDomain}" = {
+    services.caddy.virtualHosts."${cfg.url}" = {
       useACMEHost = homelab.baseDomain;
       extraConfig = ''
         reverse_proxy http://127.0.0.1:8384
