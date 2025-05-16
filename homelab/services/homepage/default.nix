eaeabfcf8f8032d1c7adbd521bbae2eb6757a12a
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-05-16 12:38:03 +0200

    refactor(homepage): add bookmarks configuration option to improve reusability

--- homelab/services/homepage/default.nix
+++ homelab/services/homepage/default.nix
@@ -7,6 +7,7 @@ let
   service = "homepage-dashboard";
   cfg = config.homelab.services.homepage;
   homelab = config.homelab;
+  bookmarks = import ./bookmarks.nix;
 in
 {
   options.homelab.services.homepage = {
@@ -35,6 +36,34 @@ in
           }
         )
       );
+    };
+
+    bookmarks = lib.mkOption {
+      type = lib.types.listOf (
+        lib.types.attrsOf (lib.types.listOf (lib.types.attrsOf (lib.types.listOf lib.types.attrs)))
+      );
+      default = import ./bookmarks.nix;
+      description = "Bookmarks to show on the homepage.";
+      example = [
+        {
+          "NixOS" = [
+            {
+              name = "NixOS";
+              href = "https://nixos.org";
+              icon = "si-nixos";
+            }
+          ];
+        }
+        {
+          "GitHub" = [
+            {
+              name = "GitHub";
+              href = "https://github.com";
+              icon = "github";
+            }
+          ];
+        }
+      ];
     };
   };
   config = lib.mkIf cfg.enable {
@@ -262,77 +291,7 @@ in
           }
         ];
 
-      bookmarks = [
-        {
-          "External Utilities" = [
-            {
-              "Google Notizen" = [
-                {
-                  abbr = "gk";
-                  href = "https://keep.google.com/";
-                  icon = "google-keep";
-                }
-              ];
-            }
-            {
-              "Public IP" = [
-                {
-                  abbr = "IP";
-                  description = "Check public IP and associated data";
-                  icon = "https://www.whatismyip.com/images/What-Is-My-IP.webp";
-                  href = "https://www.whatismyip.com/";
-                }
-              ];
-            }
-            {
-              "Who Is Lookup" = [
-                {
-                  abbr = "WIs";
-                  description = "Check ICAN info for a given IP address or domain";
-                  icon = "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_5765bb06f84bd8aa701f4f68a3ad3711/domaintools.png";
-                  href = "https://whois.domaintools.com/";
-                }
-              ];
-            }
-            {
-              "Speed Test" = [
-                {
-                  abbr = "ST";
-                  description = "Upload + download speeds and latency";
-                  icon = "mdi-speedometer";
-                  href = "https://speed.cloudflare.com/";
-                }
-              ];
-
-            }
-          ];
-        }
-        {
-          "Development" = [
-            {
-              "GitHub" = [
-                {
-                  abbr = "GH";
-                  description = "GitHub";
-                  icon = "github";
-                  href = "https://github.com/JakobLichterfeld";
-                }
-              ];
-            }
-            {
-              "GitLab" = [
-                {
-                  abbr = "GL";
-                  description = "GitLab";
-                  icon = "gitlab";
-                  href = "https://gitlab.com/GhostReader";
-                }
-              ];
-            }
-          ];
-        }
-
-      ];
+      bookmarks = cfg.bookmarks;
     };
     services.caddy.virtualHosts."${homelab.baseDomain}" = {
       useACMEHost = homelab.baseDomain;
