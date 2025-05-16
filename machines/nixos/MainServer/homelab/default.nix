f3051e0a40c7a1dddfbb28e2ff59e38d6c0938f4
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-05-16 13:37:38 +0200

    fix(homepage): add missing siteMonitor entries for additional services

--- machines/nixos/MainServer/homelab/default.nix
+++ machines/nixos/MainServer/homelab/default.nix
@@ -68,6 +68,7 @@ in
             description = "Powerline Adapter im Wohnzimmer";
             icon = "mdi-lan";
             href = "http://${machinesSensitiveVars.Powerline.ipAddressLivingRoom}";
+            siteMonitor = "http://${machinesSensitiveVars.Powerline.ipAddressLivingRoom}";
           }
           {
             category = "Other Devices";
@@ -75,6 +76,7 @@ in
             description = "Powerline Adapter im Schlafzimmer";
             icon = "mdi-lan";
             href = "http://${machinesSensitiveVars.Powerline.ipAddressBedroom}";
+            siteMonitor = "http://${machinesSensitiveVars.Powerline.ipAddressBedroom}";
           }
           {
             category = "Mobile";
@@ -82,6 +84,7 @@ in
             description = "Service to seamless share clipboard, files, notifications";
             icon = "https://play-lh.googleusercontent.com/hXPaRP8HSOYVgaMHCYx7mYOqb8hhcpVxFFz0_n61v543ZzxCME98chPwFlElv9M3D7U=w240-h480-rw";
             href = "https://joinjoaomgcd.appspot.com";
+            siteMonitor = "https://joinjoaomgcd.appspot.com";
           }
           {
             category = "Mobile";
@@ -89,20 +92,23 @@ in
             description = "Quickly send and receive WhatsApp messages right from your browser";
             icon = "whatsapp";
             href = "https://web.whatsapp.com";
+            siteMonitor = "https://web.whatsapp.com";
           }
           {
             category = "Mobile";
             name = "AirDroid ${machinesSensitiveVars.MobileMainDev.name}";
             description = "Dateiübertragung und -verwaltung";
             icon = "https://cdn.icon-icons.com/icons2/278/PNG/512/AirDroid_30195.png";
-            href = "https://${machinesSensitiveVars.MobileMainDev.ipAddress}:${machinesSensitiveVars.MobileMainDev.airDroidPort}";
+            href = "https://${machinesSensitiveVars.MobileMainDev.ipAddress}:${toString machinesSensitiveVars.MobileMainDev.airDroidPort}";
+            siteMonitor = "https://${machinesSensitiveVars.MobileMainDev.ipAddress}:${toString machinesSensitiveVars.MobileMainDev.airDroidPort}";
           }
           {
             category = "Mobile";
             name = "AirDroid ${machinesSensitiveVars.MobileMainDevTablet.name}";
             description = "Dateiübertragung und -verwaltung";
             icon = "https://cdn.icon-icons.com/icons2/278/PNG/512/AirDroid_30195.png";
-            href = "https://${machinesSensitiveVars.MobileMainDevTablet.ipAddress}:${machinesSensitiveVars.MobileMainDevTablet.airDroidPort}";
+            href = "https://${machinesSensitiveVars.MobileMainDevTablet.ipAddress}:${toString machinesSensitiveVars.MobileMainDevTablet.airDroidPort}";
+            siteMonitor = "https://${machinesSensitiveVars.MobileMainDevTablet.ipAddress}:${toString machinesSensitiveVars.MobileMainDevTablet.airDroidPort}";
           }
           {
             category = "External Services";
@@ -110,6 +116,7 @@ in
             description = "Dynamic DNS for fixed public IP";
             icon = "duckdns";
             href = "https://www.duckdns.org/domains";
+            siteMonitor = "https://www.duckdns.org/domains";
           }
           {
             category = "External Services";
@@ -117,6 +124,7 @@ in
             description = "Secure networks between devices";
             icon = "tailscale";
             href = "https://login.tailscale.com/admin/machines";
+            siteMonitor = "https://login.tailscale.com";
           }
           {
             category = "External Services";
@@ -124,6 +132,7 @@ in
             description = "Secure networks between devices";
             icon = "si-zerotier";
             href = "https://my.zerotier.com";
+            siteMonitor = "https://my.zerotier.com";
           }
           {
             category = "External Services";
@@ -131,6 +140,7 @@ in
             description = "Cron Job Monitorings";
             icon = "https://healthchecks.io/static/img/logo.svg";
             href = "https://healthchecks.io/projects/fad147f3-3d1c-4a26-8a22-48dfd032b9f5/checks/";
+            siteMonitor = "https://healthchecks.io";
           }
         ];
       };
