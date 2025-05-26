dc8658fc69050d3e3c26e597bc06c0afb7777e9a
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-05-26 10:21:54 +0200

    feat(darwin): add bundix for creating Nix packages from Gemfiles

--- machines/darwin/MainDev/packages.nix
+++ machines/darwin/MainDev/packages.nix
@@ -22,6 +22,7 @@ with pkgs;
   openssl_3
   ruby
   bundler # Ruby dependency manager
+  bundix # Creates Nix packages from Gemfiles
   rubocop # Ruby static code analyzer and formatter
   devenv # Fast, Declarative, Reproducible, and Composable Developer Environments
   nil # Nix Language server: An incremental analysis assistant for writing in Nix
