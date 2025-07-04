59591100fc0c8fbc01e7acb8d911affb9ee886df
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-07-04 19:05:03 +0200

    feat(tailscale): allow optional exit node advertisement

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -4,6 +4,9 @@
   lib,
   ...
 }:
+let
+  advertiseExitNode = lib.hasInfix "server" (lib.toLower config.networking.hostName);
+in
 {
   environment.systemPackages = [ pkgs.tailscale ];
 
@@ -13,5 +16,14 @@
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets.tailscaleAuthKey.path;
+
+    useRoutingFeatures = if advertiseExitNode then "both" else "client";
+    permitCertUid = if config.services.caddy.enable then "caddy" else null; # Allow the Caddy user(and service) to edit certs, see https://wiki.nixos.org/wiki/Tailscale#Configuring_TLS
+
+    extraUpFlags = lib.optionals (advertiseExitNode) [
+      "--advertise-exit-node"
+      "--exit-node-allow-lan-access"
+      "--reset"
+    ];
   };
 }
