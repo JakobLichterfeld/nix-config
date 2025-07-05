a604d0a7e6c80cc7f522efa6fd4d094e8b00e2d3
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-07-05 13:42:35 +0200

    perf(tailscale): optimize UDP throughput for exit nodes

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -6,12 +6,61 @@
 }:
 let
   advertiseExitNode = lib.hasInfix "server" (lib.toLower config.networking.hostName);
+  possibleIfaces = [
+    "enp1s0"
+    "enp2s0"
+    "eth0"
+    "eno1"
+    "eth0"
+    "eth1"
+  ];
+  existingIfaces = builtins.filter (iface: config.networking.interfaces ? ${iface}) possibleIfaces;
+  mainInterface = if existingIfaces != [ ] then builtins.head existingIfaces else "eth0";
 in
 {
-  environment.systemPackages = [ pkgs.tailscale ];
+  environment.systemPackages =
+    [ pkgs.tailscale ]
+    ++ lib.optionals advertiseExitNode [
+      pkgs.ethtool
+    ]
+    ++ lib.optionals (advertiseExitNode && config.networking.useNetworkd) [
+      pkgs.networkd-dispatcher
+    ];
 
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+
+  # optimize UDP throughput, see https://wiki.nixos.org/wiki/Tailscale#Optimize_the_performance_of_subnet_routers_and_exit_nodes and https://tailscale.com/kb/1320/performance-best-practices#ethtool-configuration
+  services.networkd-dispatcher = lib.mkIf (advertiseExitNode && config.networking.useNetworkd) {
+    enable = true;
+    rules."50-tailscale" = {
+      onState = [ "routable" ];
+      script = ''
+        for iface in ${builtins.toString possibleIfaces}; do
+          if [[ -e /sys/class/net/$iface ]]; then
+            ${lib.getExe pkgs.ethtool} -K $iface rx-udp-gro-forwarding on rx-gro-list off
+          fi
+        done
+      '';
+    };
+  };
+  systemd.services.ethtool-tuning = lib.mkIf (advertiseExitNode && !config.networking.useNetworkd) {
+    description = "Apply ethtool tuning to interfaces to optimize UDP throughput for Tailscale exit nodes";
+    after = [ "network-online.target" ];
+    before = [ "tailscaled.service" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "ethtool-tune" ''
+        for iface in ${builtins.toString possibleIfaces}; do
+          if [[ -e /sys/class/net/$iface ]]; then
+            ${lib.getExe pkgs.ethtool} -K $iface rx-udp-gro-forwarding on rx-gro-list off
+          fi
+        done
+      '';
+    };
+  };
 
   services.tailscale = {
     enable = true;
