b2cecc7a894f9a740d08e502de6b7bdb93eef066
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-07-31 09:19:40 +0200

    refactor(dock): update Dock setup script to use sudo for user execution to avoid context switch

--- modules/darwin/dock/default.nix
+++ modules/darwin/dock/default.nix
@@ -92,18 +92,18 @@ in
     in
     {
       system.activationScripts.postActivation.text = ''
-          echo >&2 "Setting up the Dock for ${cfg.username}..."
-          su ${cfg.username} -s /bin/sh <<'USERBLOCK'
-        haveURIs="$(${dockutil}/bin/dockutil --list | ${pkgs.coreutils}/bin/cut -f2)"
-        if ! diff -wu <(echo -n "$haveURIs") <(echo -n '${wantURIs}') >&2 ; then
-          echo >&2 "Resetting Dock."
-          ${dockutil}/bin/dockutil --no-restart --remove all
-          ${createEntries}
-          killall Dock
-        else
-          echo >&2 "Dock setup complete."
-        fi
-        USERBLOCK
+        echo >&2 "Scheduling Dock setup for ${cfg.username}..."
+        sudo -u ${cfg.username} ${pkgs.writeShellScript "dock-setup" ''
+          haveURIs="$(${dockutil}/bin/dockutil --list | ${pkgs.coreutils}/bin/cut -f2)"
+          if ! diff -wu <(echo -n "$haveURIs") <(echo -n '${wantURIs}') >&2 ; then
+            echo >&2 "Resetting Dock."
+            ${dockutil}/bin/dockutil --no-restart --remove all
+            ${createEntries}
+            killall Dock
+          else
+            echo >&2 "Dock setup complete."
+          fi
+        ''}
       '';
     }
   );
