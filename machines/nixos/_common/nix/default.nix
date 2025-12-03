64b24efcff642c97004a5fda3ce7f8a679d70ca0
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-12-03 16:46:07 +0100

    feat(nix): Improve garbage collection and store optimization
    This commit enhances the Nix store management by making garbage collection (GC) and optimization more frequent and robust.
    
    Changes include:
    - Changed `nix.gc.dates` from `weekly` to `daily` to ensure more regular cleanup.
    - Reduced the retention period for GC options from `30d` to `14d` (`--delete-older-than`).
    - Added `nix.gc.persistent = true` to ensure GC runs catch up after system downtime.
    - Changed `nix.optimise.dates` from `weekly` to `daily` for more frequent store optimization.
    
    These adjustments aim to reduce disk space usage, maintain store efficiency, and ensure scheduled maintenance tasks are reliably executed.

--- machines/nixos/_common/nix/default.nix
+++ machines/nixos/_common/nix/default.nix
@@ -2,11 +2,12 @@
 {
   nix.gc = {
     automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+    dates = "daily";
+    options = "--delete-older-than 14d";
+    persistent = true; # catch up on missed runs of the service when the system was powered down.
   };
   nix.optimise.automatic = true;
-  nix.optimise.dates = [ "weekly" ];
+  nix.optimise.dates = [ "daily" ];
 
   nix.settings.experimental-features = lib.mkDefault [
     "nix-command"
