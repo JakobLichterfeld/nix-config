21d2d9b09eeb804fa8ce29620d790f39b22f44bd
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2026-05-25 09:28:23 +0200

    feat(teslamate-abrp): update teslamate-abrp to version 3.4.3

--- homelab/services/teslamate-abrp/default.nix
+++ homelab/services/teslamate-abrp/default.nix
@@ -8,7 +8,7 @@ let
   service = "teslamate-abrp";
   cfg = config.homelab.services.teslamate-abrp;
   homelab = config.homelab;
-  teslamate-abrp-version = "3.4.1";
+  teslamate-abrp-version = "3.4.3";
 in
 {
   options.homelab.services.${service} = {
